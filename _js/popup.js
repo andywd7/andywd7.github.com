004e375a3669f3eb5551da8b0c4470bcea0e7e98
Andy Denham <andywd7@users.noreply.github.com>
2016-04-06 08:49:02 +0100

    Update popup.js

--- _js/popup.js
+++ _js/popup.js
@@ -1,16 +1,15 @@
-function windowPopup(url, w, h) {
+function windowPopup(url,w,h) {
     var left = (screen.width / 2) - (w / 2),
         top = (screen.height / 2) - (h / 2);
-
     window.open(url, 'name', 'menubar=0,toolbar=0,resizable=1,scrollbars=1,width=' + w + ',height=' + h + ',top=' + top + ',left=' + left);
 };
 
-var classname = document.getElementsByClassName('share'),
-    myFunction = function(e) {
+var classname = document.getElementsByClassName('link--share');
+var clickOpen = function(e) {
         e.preventDefault();
-        windowPopup(this.getAttribute('href'), 650, 450);
+        windowPopup(this.getAttribute('href'), 500, 400);
     };
 
 for (var i = 0; i < classname.length; i++) {
-    classname[i].addEventListener('click', myFunction, false);
+    classname[i].addEventListener('click', clickOpen, false);
 };
